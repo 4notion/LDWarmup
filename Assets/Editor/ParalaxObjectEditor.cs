1e2d32caea035df72f6c5adfd49acaebf6f586ef
4notion <ahajden@gmail.com>
2013-08-21 13:57:34 +0200

    initial commit

---
+++ Assets/Editor/ParalaxObjectEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(ParalaxObject))]
+public class ParalaxObjectEditor : Editor {
+
+	private ParalaxObject that;
+
+    void Awake()
+    {
+        that = (ParalaxObject)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        that.speed.x = EditorGUILayout.Slider("Paralax Speed X:", that.speed.x, -1.0f, 1.0f);
+        that.speed.y = EditorGUILayout.Slider("Paralax Speed Y:", that.speed.y, -1.0f, 1.0f);
+    }
+}
---
+++ Assets/Scripts/Utility/ParalaxObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParalaxObject : MonoBehaviour {
+
+    public Vector3 startingPosition;
+
+    public Vector2 speed = new Vector2();
+
+    void Start()
+    {
+        startingPosition = this.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPosition = new Vector3();
+
+        newPosition.x = Camera.main.transform.position.x * speed.x * -1 + startingPosition.x;
+        newPosition.y = Camera.main.transform.position.y * speed.y * -1 + startingPosition.y;
+        newPosition.z = startingPosition.z;
+
+        this.transform.position = newPosition;
+	}
+}
